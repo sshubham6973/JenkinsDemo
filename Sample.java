4772fadc319af9c5e31f8c74fdc1a59dcb15344e
Shubham Singh <sshubham6973@gmail.com>
2022-07-14 17:22:15 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -2,6 +2,7 @@ class Sample{
   public static void main(String[] args){
     System.out.println("Hello from Jenkins");
     System.out.println("Edit done at 5:14 PM");
+    System.out.println("Edit done at 5:21 PM");
   }
 
 }
